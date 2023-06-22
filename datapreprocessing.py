dea886acc286dd044dc7abf2c46f7ce13c344822
shubhamdv1 <shubhamdilip.vhanmane@cloudangles.com>
2023-06-22 12:34:28 +0530

    featureengineering
    Added new featureengineering file

--- datapreprocessing.py
+++ datapreprocessing.py
@@ -2,18 +2,13 @@ from dataanalysis import data_analysis
 import pandas as pd
 def data_preprocessing():
     dataset = data_analysis()
-    dataset['date_parsed'] = pd.to_datetime(dataset['Date'],format="%Y-%m-%d")
-    dataset['day_of_month'] = (dataset['date_parsed'].dt.day).astype(int)
-    dataset['month_of_year'] = (dataset['date_parsed'].dt.month).astype(int)
-    dataset['year'] = (dataset['date_parsed'].dt.year).astype(int)
-
+    
     # Remove duplicates
     dataset.drop_duplicates(inplace=True)  # Remove duplicate rows
 
     # Drop columns with a high percentage of missing values
     threshold = 0.8  # Set your desired threshold
     dataset.dropna(thresh=threshold*len(dataset), inplace=True, axis=1)
-    dataset.drop(['Date'], inplace=True, axis=1)
 
     # Fill missing values in numerical columns with the mean
     dataset.fillna(dataset.mean(numeric_only=True), inplace=True)
---
+++ featureengineering.py
@@ -0,0 +1,21 @@
+from sklearn import preprocessing
+le = preprocessing.LabelEncoder()
+import pandas as pd
+from datapreprocessing import data_preprocessing
+def feature_engineering():
+    dataset = data_preprocessing()
+    import pandas as pd
+    # Convert 'Date' column to datetime
+    dataset['date_parsed'] = pd.to_datetime(dataset['Date'], format="%Y-%m-%d")
+    
+    # Extract additional features from 'date_parsed'
+    dataset['day_of_month'] = dataset['date_parsed'].dt.day
+    dataset['month_of_year'] = dataset['date_parsed'].dt.month
+    dataset['year'] = dataset['date_parsed'].dt.year
+    
+    dataset.drop(['Date'], inplace=True, axis=1)
+    
+    dataset.to_csv("Gold_cleaned_dataset.csv", index = False)
+    return dataset
+
+feature_engineering()
